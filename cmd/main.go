23da1f244c57464eaf7c15934f8d1a4a0e54ef7a
westcrime <westcrime77@rambler.ru>
2025-09-10 15:27:46 +0300

    implement simple grpc api

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,56 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 
-	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	desc "github.com/westcrime/chat-server/pkg/chat_v1"
 )
 
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedChatV1Server
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Chat id: %d", req.GetId())
+
+	return &emptypb.Empty{}, nil
+}
+
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("Chat id: %+v", req.Info)
+
+	return &desc.CreateResponse{
+		Id: 1}, nil
+}
+
+func (s *server) SendMessage(ctx context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
+	log.Printf("Chat message text: %s", req.Text)
+
+	return &emptypb.Empty{}, nil
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterChatV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,19 @@
-module chat-server
+module github.com/westcrime/chat-server
 
 go 1.24.6
 
 require (
-	github.com/fatih/color v1.18.0 // indirect
+	github.com/fatih/color v1.18.0
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.36.9
+)
+
+require (
+	github.com/brianvoe/gofakeit v2.2.0+incompatible
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
 )
